73d70d71cba3361b9a5fabdc47dd2a2c6f6c3049
LetsCodeSomething <mishaLySL@yandex.ru>
2024-05-20 17:49:24 +1000

    Add graph module

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './App';
 import './index.css';
 import dataset from './dataset/Dataset';
 import {Table} from "./components/Table";
+import {Graph} from "./components/Graph";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,5 +16,6 @@ root.render(
 
 root.render(<>
     <h3>Данные по продажам в Walmart</h3>
+    <Graph/>
     <Table dataset={dataset} rowsPerPage="25" selectedPage="0"/>
 </>);
